1ad1bb0dcae3974b7a1e6c9d6e29f6a686738209
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-06-30 17:33:56 +0530

    Navbar is completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,18 +7,20 @@ import { GrTasks } from "react-icons/gr";
 import { FaPen } from "react-icons/fa6";
 import { FaHandsHelping } from "react-icons/fa";
 import { IoIosCall } from "react-icons/io";
-
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [active, setActive] = useState('#');
+
   return (
     <nav>
-      <a href="#"><AiFillHome /></a>
-      <a href="#about"><FaUser/></a>
-      <a href="#education"><IoMdSchool/></a>
-      <a href="#projects"><GrTasks/></a>
-      <a href="#blogs"><FaPen/></a>
-      <a href="#services"><FaHandsHelping/></a>
-      <a href="#contact"><IoIosCall/></a>
+      <a href="#" onClick = {() => setActive('#')} className={active === '#' ? 'active' : ''}><AiFillHome /></a>
+      <a href="#about" onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}><FaUser/></a>
+      <a href="#education" onClick={() => setActive('#education')} className={ active === '#education' ? 'active' : ''}><IoMdSchool/></a>
+      <a href="#projects" onClick={() => setActive('#projects')} className={ active === "#projects"? 'active' : ''}><GrTasks/></a>
+      <a href="#blogs" onClick={() => setActive('#blogs')} className={ active === "#blogs"? 'active' : ''}><FaPen/></a>
+      <a href="#services" onClick={() => setActive('#services')} className={ active === "#services"? 'active' : ''}><FaHandsHelping/></a>
+      <a href="#contact" onClick={() => setActive('#contact')} className={ active === "#contact"? 'active' : ''}><IoIosCall/></a>
     </nav>
   )
 }
