9b15ee869cbec7eb566def7f880ce4b0e3572201
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-06-30 13:42:53 +0530

    create the structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Contact from './components/contact/Contact';
+import Projects from './components/projects/Projects';
+import About from './components/about/About';
+import Blogs from './components/blogs/Blogs';
+import Services from './components/services/Services';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Education from './components/education/Education';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Education/>
+        <Projects/>
+        <Services/>
+        <Blogs/>
+        <Contact/>
+        <Footer/>
+         
     </div>
   );
 }
