11760201b36a034284ff9902fa89d00bbe3a6d37
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-07-06 20:46:40 +0530

    Skills section updated

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
-import { BsFillPatchCheckFill } from 'react-icons/bs'
+import React, { useEffect, useRef } from "react";
+import {
+  RiReactjsLine,
+  RiNextjsFill
+} from "react-icons/ri";
+import { 
+  FaNodeJs, 
+  FaPython, 
+  FaJava, 
+  FaHtml5, 
+  FaCss3Alt, 
+  FaGitAlt 
+} from "react-icons/fa";
+import { BiLogoMongodb } from "react-icons/bi";
+import {
+  SiSpringboot, 
+  SiMysql, 
+  SiTensorflow, 
+  SiJupyter, 
+  SiPhp, 
+  SiXampp, 
+  SiTypescript 
+} from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
 import './skills.css'
 
 const Skills = () => {
@@ -10,99 +32,57 @@ const Skills = () => {
 
       {/*programming languages*/}
 
-      <div className="container experience_container">
-        <div className="experience_frontend">
-          <h3>Programming Languages</h3>
-          <div className="experience_content">
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>C++</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>JAVA</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>Python</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>C#</h4>
-                <small className='text-light'>Beginner</small>
-              </div>
-            </article>
-
-          </div>
+      <div className="container icons-container">
+        <div className="tech-icon cyan">
+          <RiReactjsLine />
         </div>
-
-        {/*Experience in */}
-
-        <div className="experience_backend">
-          <h3>Experience In</h3>
-          <div className="experience_content">
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>AZURE</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>MySql</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>Linux</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>Git</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsFillPatchCheckFill className='experience_details-icon' />
-              <div>
-                <h4>MongoDb</h4>
-                <small className='text-light'>Experience</small>
-              </div>
-            </article>
-
-          </div>
+        <div className="tech-icon green">
+          <FaNodeJs />
+        </div>
+        <div className="tech-icon">
+          <RiNextjsFill />
+        </div>
+        <div className="tech-icon orange">
+          <SiTensorflow />
+        </div>
+        <div className="tech-icon orange">
+          <FaJava />
+        </div>
+        <div className="tech-icon blue">
+          <FaPython />
+        </div>
+        <div className="tech-icon green">
+          <BiLogoMongodb />
+        </div>
+        <div className="tech-icon green">
+          <SiSpringboot />
+        </div>
+        <div className="tech-icon blue">
+          <SiMysql />
+        </div>
+        <div className="tech-icon orange">
+          <FaHtml5 />
+        </div>
+        <div className="tech-icon blue">
+          <FaCss3Alt />
+        </div>
+        <div className="tech-icon yellow">
+          <IoLogoJavascript />
+        </div>
+        <div className="tech-icon blue">
+          <SiTypescript />
+        </div>
+        <div className="tech-icon orange">
+          <SiJupyter />
+        </div>
+        <div className="tech-icon orange">
+          <FaGitAlt />
+        </div>
+        <div className="tech-icon blue">
+          <SiPhp />
+        </div>
+        <div className="tech-icon orange">
+          <SiXampp />
         </div>
       </div>
     </section>
