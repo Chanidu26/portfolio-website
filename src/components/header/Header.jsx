809b68cee8f75e062da8902cda9a98650b3df09a
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-08-04 13:39:30 +0530

    type writer effect added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,16 +4,24 @@ import CTA from './CTA';
 import me from '../../assets/Header.jpg';
 import Socials from './Socials';
 import { FaAngleDoubleDown } from "react-icons/fa";
-
+import { useTypewriter, Cursor } from "react-simple-typewriter"
 
 
 const Header = () => {
+  const [ text ] = useTypewriter({
+    words: ['Undergraduate', 'Developer', 'Engineer', 'Designer', 'Blogger'],
+    loop: true,
+    typeSpeed: 100,
+    deleteSpeed: 50,
+  });
   return (
     <header>
     <div className="container header_container">
       <h5>Hello I'm</h5>
       <h1>Chanidu Karunarathna</h1>
-      <h5 className="text-light">Undergradute Fullstack Engineer | Developer | Graphic Designer | Blogger</h5>
+      <h3 className="text-light">
+          I am a <span style={{ fontWeight: 'bold'}}>{text}</span><Cursor />
+      </h3>
       <Socials/>
       <CTA/>
       <div className='me'>
